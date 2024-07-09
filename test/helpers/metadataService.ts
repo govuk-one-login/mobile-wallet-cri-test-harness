d73fe599f604d94763be8e4c2374dd4b4675b2de
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-07-09 12:16:45 +0100

    feat(DCMAW-9587): add service for validating the credential metadata

---
+++ test/helpers/metadataService.ts
@@ -0,0 +1,78 @@
+import axios, { AxiosResponse } from "axios";
+import { parseAsUrl } from "./parseAsUrl";
+
+export class MetadataService {
+  static #instance: MetadataService;
+  private METADATA_PATH: string = ".well-known/openid-credential-issuer";
+  private _getCredentialsEndpoint!: string;
+  private _getAuthorizationServersEndpoint!: string;
+
+  private constructor() {}
+
+  public static get instance(): MetadataService {
+    if (!MetadataService.#instance) {
+      MetadataService.#instance = new MetadataService();
+    }
+    return MetadataService.#instance;
+  }
+
+  get getCredentialsEndpoint(): string {
+    return this._getCredentialsEndpoint;
+  }
+
+  get getAuthorizationServersEndpoint(): string {
+    return this._getAuthorizationServersEndpoint;
+  }
+
+  async validate(criDomain: string) {
+    const metadataResponse = await this.getMetadata(criDomain);
+
+    if (metadataResponse.status !== 200) {
+      throw new Error("INVALID_STATUS_CODE");
+    }
+    const metadata = metadataResponse.data;
+
+    const credentialsEndpoint = metadata["credentials_endpoint"];
+    if (!credentialsEndpoint) {
+      throw new Error("INVALID_CREDENTIALS_ENDPOINT");
+    }
+
+    this._getCredentialsEndpoint = parseAsUrl(credentialsEndpoint).toString();
+
+    const authorizationServers = metadata["authorization_servers"];
+    if (
+      !authorizationServers ||
+      !Array.isArray(authorizationServers) ||
+      authorizationServers.length < 1
+    ) {
+      throw new Error("INVALID_AUTHORIZATION_SERVERS");
+    }
+    this._getAuthorizationServersEndpoint = parseAsUrl(
+      authorizationServers[0],
+    ).toString();
+
+    const credentialIssuer = metadata["credential_issuer"];
+    if (!credentialIssuer) {
+      throw new Error("INVALID_CREDENTIAL_ISSUER");
+    }
+    parseAsUrl(credentialIssuer);
+
+    const credentialConfigurations =
+      metadata["credential_configurations_supported"];
+    if (Object.keys(credentialConfigurations).length === 0) {
+      throw new Error("INVALID_CREDENTIAL_CONFIGURATIONS_SUPPORTED");
+    }
+
+    return true;
+  }
+
+  private async getMetadata(domain): Promise<AxiosResponse> {
+    try {
+      const metadataUrl = new URL(this.METADATA_PATH, domain).toString();
+      return await axios.get(metadataUrl);
+    } catch (error) {
+      console.log(error);
+      throw new Error("GET_METADATA_ERROR");
+    }
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,32 @@
-import { getCredentialOffer } from "../src/config";
+import { getCredentialOffer, getCriDomain } from "../src/config";
 import { CredentialOfferService } from "./helpers/credentialOfferService";
+import { MetadataService } from "./helpers/metadataService";
 
 let PREAUTHORIZED_CODE: string;
+let CREDENTIALS_ENDPOINT: string;
+let AUTHORIZATION_SERVER: string;
 
 describe("tests", () => {
   console.log("Running tests");
+  const credentialOfferDeepLink = getCredentialOffer();
+  const criDomain = getCriDomain();
 
-  it("should validate the credential offer", async () => {
+  it("should validate the credential offer", () => {
     const credentialOfferService = CredentialOfferService.instance;
-    const credentialOfferDeepLink = getCredentialOffer();
-
     expect(credentialOfferService.validate(credentialOfferDeepLink)).toEqual(
       true,
     );
     PREAUTHORIZED_CODE = credentialOfferService.preAuthorizedCode;
     console.log(PREAUTHORIZED_CODE);
+  });
+
+  it("should validate the credential metadata", async () => {
+    const metadataService = MetadataService.instance;
+    expect(await metadataService.validate(criDomain)).toEqual(true);
+
+    CREDENTIALS_ENDPOINT = metadataService.getCredentialsEndpoint;
+    console.log(CREDENTIALS_ENDPOINT);
+    AUTHORIZATION_SERVER = metadataService.getAuthorizationServersEndpoint;
+    console.log(AUTHORIZATION_SERVER);
   });
 });
