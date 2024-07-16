e73a2b920055a2bf333902513aeb07f761539cce
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-07-16 17:37:37 +0100

    feat(DCMAW-9591): add pre-auth code schemas

---
+++ test/helpers/preAuthorizedCode/headerSchema.ts
@@ -0,0 +1,18 @@
+export const headerSchema = {
+  type: "object",
+  properties: {
+    kid: {
+      type: "string",
+      minLength: 1,
+    },
+    typ: {
+      type: "string",
+      enum: ["JWT"],
+    },
+    alg: {
+      type: "string",
+      enum: ["ES256"],
+    },
+    additionalProperties: false,
+  },
+};
---
+++ test/helpers/preAuthorizedCode/payloadSchema.ts
@@ -0,0 +1,30 @@
+export const payloadSchema = {
+  type: "object",
+  properties: {
+    aud: {
+      type: "string",
+      enum: ["urn:fdc:gov:uk:wallet"],
+    },
+    clientId: {
+      type: "string",
+      enum: ["EXAMPLE-CRI"],
+    },
+    iss: {
+      type: "string",
+      enum: ["urn:fdc:gov:uk:example-credential-issuer"],
+    },
+    credential_identifiers: {
+      type: "array",
+      items: { type: "string", format: "uuid" },
+      minItems: 1,
+      maxItems: 1,
+    },
+    iat: {
+      type: "number",
+    },
+    exp: {
+      type: "number",
+    },
+    additionalProperties: false,
+  },
+};
