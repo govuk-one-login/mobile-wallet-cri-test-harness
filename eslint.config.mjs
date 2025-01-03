ba16272af66a0e637d976b6348c26dd0caa2303d
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-01-03 10:20:27 +0000

    build: move to eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    ignores: [
+      "dist",
+      "coverage"
+    ],
+  },
+  {
+    files: ['**/*.ts'],
+  },
+  eslint.configs.recommended, ...tseslint.configs.strict, ...tseslint.configs.stylistic,
+  {
+    rules: {
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-require-imports": "off"
+    }
+  },
+]
