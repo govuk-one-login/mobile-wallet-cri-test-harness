ee2a7da48de2b3407b51d3287b21b2300aa2c39d
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-07-03 10:55:28 +0100

    test: update coverage report path

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ export default {
   },
   reporters: [
     'default',
-    ['jest-junit', { outputDirectory: 'results', outputName: 'report.xml' }]
+    ['jest-junit', { outputDirectory: '/results', outputName: 'report.xml' }]
   ],
   testMatch: ['**/*.test.ts'],
   testEnvironment: 'node',
@@ -15,6 +15,6 @@ export default {
     'test/**',
     'src/**',
   ],
-  coverageDirectory: 'coverage',
+  coverageDirectory: '/results/coverage',
   coverageProvider: 'v8',
 }
