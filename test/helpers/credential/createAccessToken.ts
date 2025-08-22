b03d16c577f62635cb13d0565fab813cb213118b
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-08-22 18:17:14 +0100

    fix: add exp and jti claims to mock access token

--- test/helpers/credential/createAccessToken.ts
+++ test/helpers/credential/createAccessToken.ts
@@ -1,5 +1,6 @@
 import { importJWK, SignJWT, JWK, JWTPayload } from "jose";
 import { getKeyId } from "../../../src/config";
+import { randomUUID } from "node:crypto";
 
 export interface AccessToken {
   access_token: string;
@@ -8,6 +9,7 @@ export interface AccessToken {
 }
 
 const SIGNING_ALGORITHM = "ES256";
+const TTL = 180;
 
 export async function createAccessToken(
   c_nonce: string,
@@ -20,7 +22,7 @@ export async function createAccessToken(
     credential_identifiers: preAuthorizedCodePayload.credential_identifiers!,
     c_nonce: c_nonce,
   };
-
+  const nowInSeconds = Math.floor(Date.now() / 1000);
   const accessToken = await new SignJWT(customClaims)
     .setProtectedHeader({
       alg: SIGNING_ALGORITHM,
@@ -30,11 +32,13 @@ export async function createAccessToken(
     .setSubject(walletSubjectId)
     .setIssuer(preAuthorizedCodePayload.aud! as string)
     .setAudience(preAuthorizedCodePayload.iss!)
+    .setJti(randomUUID())
+    .setExpirationTime(nowInSeconds + TTL)
     .sign(signingKeyAsKeyLike);
 
   return {
     access_token: accessToken,
     token_type: "bearer",
-    expires_in: 180,
+    expires_in: TTL,
   };
 }
