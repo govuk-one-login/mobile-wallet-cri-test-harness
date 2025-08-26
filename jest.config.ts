d22c2ef2437426cf8d8dd9032f4ce886e0987eb9
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-08-26 10:29:09 +0100

    test: update Jest config to work with ESM only packages

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 export default {
   transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest'
+    '^.+\\.(js)$': [
+      'ts-jest',
     ],
   },
   reporters: [
@@ -17,12 +17,13 @@ export default {
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  // By default, Jest ignores node_modules when transforming files.
-  // This line makes an exception for certain modern packages that ship only ESM
-  // so ts-jest will transpile them to CommonJS for Jest to run.
+  /*
+  By default, Jest skips transforming files in node_modules, assuming they're CommonJS-compatible.
+  Some modern packages (cbor2, @cto.af/wtf8, jose v6) ship only ESM syntax, which Jest can't execute.
+  The line below instructs Jest to transform these specific modules to CommonJS so ts-jest can process them.
+  */
   transformIgnorePatterns: [
     '/node_modules/(?!cbor2|@cto.af/wtf8|jose)'
   ],
-  // Use ts-jest preset for mixed JS/TS in ESM mode
-  preset: 'ts-jest/presets/js-with-ts-esm',
+  preset: 'ts-jest',
 }
