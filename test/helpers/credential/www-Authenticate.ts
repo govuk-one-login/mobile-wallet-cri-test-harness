e6b5350ba5b7c2d660d6e9fd85411d7e78c60368
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2025-09-30 17:11:26 +0100

    style: remove space

--- test/helpers/credential/www-Authenticate.ts
+++ test/helpers/credential/www-Authenticate.ts
@@ -2,6 +2,5 @@ export function wwwAuthenticateHeaderContainsCorrectError(
   header: string,
 ): boolean {
   if (!header.startsWith("Bearer")) return false;
-
   return header.includes('error="invalid_token"');
 }
