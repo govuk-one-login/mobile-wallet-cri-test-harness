bfe25b5556c22bf1f9f54e698a3c75f94d25ed47
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2025-09-30 17:07:07 +0100

    refactor: remove check for Array value

--- test/helpers/credential/www-Authenticate.ts
+++ test/helpers/credential/www-Authenticate.ts
@@ -1,9 +1,7 @@
 export function wwwAuthenticateHeaderContainsCorrectError(
   header: string,
 ): boolean {
-  const headerString = Array.isArray(header) ? header.join(", ") : header;
-  const normalisedHeader = headerString.trim();
-  if (!normalisedHeader.startsWith("Bearer")) return false;
+  if (!header.startsWith("Bearer")) return false;
 
-  return normalisedHeader.includes('error="invalid_token"');
+  return header.includes('error="invalid_token"');
 }
