dc0d7630e97721a3d141783580343c7d8d5b0e8c
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-12-15 16:51:53 +0000

    chore: remove logs from run-server-and-tests.sh

--- run-server-and-tests.sh
+++ run-server-and-tests.sh
@@ -2,19 +2,15 @@
 
 set -e
 
-echo "Starting server"
 bash run-server.sh &
 SERVER_PID=$!
 
 sleep 5
 
-echo "Running tests against the credential issuer"
 bash run-tests.sh
 TEST_EXIT_CODE=$?
 
-echo "Stopping server"
 kill $SERVER_PID
 wait $SERVER_PID 2>/dev/null || true
 
-echo "Exiting with code $TEST_EXIT_CODE"
 exit $TEST_EXIT_CODE
