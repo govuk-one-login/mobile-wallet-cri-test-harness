dffa4d79bf44f75385e835a124dad599b1dca6bd
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2026-01-12 12:28:27 +0000

    feat: validate metadata credential configuration supported  (#372)
    * feat: add credential configurations supported schema
    
    * refactor: rearrange metadata schema
    
    * feat: check if credential is in credential_configurations_supported
    
    * refactor: improve type safety and schema registration
    
    * test: add unit tests for metadata schema
    
    * refactor: rename new schema file and its test file
    
    * refactor: rename rulesValidator to validator
    
    * refactor: simplify logic to extract properties from credential offer
    
    * feat: validate notification endpoint only when issuer implements it
    
    * refactor: group test cases in credentialConfigurationsSupportedSchema.test.ts
    
    * refactor: group test cases in metadataSchema.test.ts
    
    * refactor: change isValidMetadata to accept an options object
    
    * test: add test cases for configuration with multiple credentials
    
    * feat: allow additional properties in objects

--- test/helpers/metadata/isValidMetadata.test.ts
+++ test/helpers/metadata/isValidMetadata.test.ts
@@ -1,107 +1,262 @@
 import { isValidMetadata } from "./isValidMetadata";
+import { getAjvInstance } from "../ajv/ajvInstance";
+import { ErrorObject, ValidateFunction } from "ajv";
 
-const authServerUrl = "https://test-auth-server.gov.uk";
-const criUrl = "https://test-example-cri.gov.uk";
+const criUrl = "https://cri.example.com";
+const authServerUrl = "https://auth.example.com";
+const credentialFormat = "jwt";
+const credentialConfigurationId = "TestCredential";
+const hasNotificationEndpoint = false;
+
+jest.mock("../ajv/ajvInstance", () => ({
+  getAjvInstance: jest.fn(),
+}));
 
 describe("isValidMetadata", () => {
-  it("should return true when metadata is valid - JWT credential", async () => {
-    const metadata = metadataBuilder().withDefaults();
-    expect(
-      await isValidMetadata(metadata, criUrl, authServerUrl, "jwt"),
-    ).toEqual(true);
-  });
-
-  it("should return true when metadata is valid - mDoc credential", async () => {
-    const metadata = metadataBuilder().withOverrides({
-      mdoc_iacas_uri: "https://test-example-cri.gov.uk/iacas",
+  beforeEach(() => {
+    jest.clearAllMocks();
+    (getAjvInstance as jest.Mock).mockReturnValue({
+      addSchema: jest.fn().mockReturnThis(),
+      compile: jest.fn().mockReturnValue(jest.fn().mockReturnValue(true)),
     });
-    expect(
-      await isValidMetadata(metadata, criUrl, authServerUrl, "mdoc"),
-    ).toEqual(true);
   });
 
-  it("should return true when metadata is valid - notification_endpoint is present", async () => {
-    const metadata = metadataBuilder().withOverrides({
-      notification_endpoint: "https://test-example-cri.gov.uk/notification",
-    });
-    expect(
-      await isValidMetadata(metadata, criUrl, authServerUrl, "jwt"),
-    ).toEqual(true);
-  });
+  it("should throw 'INVALID_METADATA' error when metadata does not comply with schema", async () => {
+    const mockValidator = jest
+      .fn()
+      .mockReturnValue(false) as unknown as ValidateFunction;
+    mockValidator.errors = [
+      { message: "mock AJV error" } as unknown as ErrorObject,
+    ];
+    const mockAjv = {
+      addSchema: jest.fn().mockReturnThis(),
+      compile: jest.fn().mockReturnValue(mockValidator),
+    };
+    (getAjvInstance as jest.Mock).mockReturnValue(mockAjv);
 
-  it("should throw 'INVALID_METADATA' error when 'credential_configurations_supported' is missing", async () => {
     const metadata = metadataBuilder().withOverrides({
       credential_configurations_supported: false,
     });
+
     await expect(
-      isValidMetadata(metadata, criUrl, authServerUrl, "jwt"),
+      isValidMetadata({
+        metadata,
+        criUrl,
+        authServerUrl,
+        credentialFormat,
+        credentialConfigurationId,
+        hasNotificationEndpoint,
+      }),
     ).rejects.toThrow(
-      'INVALID_METADATA: Metadata does not comply with the schema. [{"instancePath":"/credential_configurations_supported","schemaPath":"#/properties/credential_configurations_supported/type","keyword":"type","params":{"type":"object"},"message":"must be object"}]',
+      'INVALID_METADATA: Metadata does not comply with the schema. [{"message":"mock AJV error"}]',
     );
   });
 
-  it("should throw 'INVALID_METADATA' error when 'credential_issuer' does not match CRI URL", async () => {
+  it("should throw 'INVALID_METADATA' error when 'credential_issuer' is invalid", async () => {
     const metadata = metadataBuilder().withOverrides({
       credential_issuer: "https://something-else.com/",
     });
+
     await expect(
-      isValidMetadata(metadata, criUrl, authServerUrl, "jwt"),
+      isValidMetadata({
+        metadata,
+        criUrl,
+        authServerUrl,
+        credentialFormat,
+        credentialConfigurationId,
+        hasNotificationEndpoint,
+      }),
     ).rejects.toThrow(
-      'INVALID_METADATA: Invalid "credential_issuer" value. Should be https://test-example-cri.gov.uk but found https://something-else.com/',
+      'INVALID_METADATA: Invalid "credential_issuer" value. Should be https://cri.example.com but found https://something-else.com/',
     );
   });
 
-  it("should throw 'INVALID_METADATA' error when 'credential_endpoint' does not match CRI's credential endpoint", async () => {
-    const metadata = metadataBuilder().withOverrides({
-      credential_endpoint: "https://something-else.com/something",
-    });
-    await expect(
-      isValidMetadata(metadata, criUrl, authServerUrl, "jwt"),
-    ).rejects.toThrow(
-      'INVALID_METADATA: Invalid "credential_endpoint" value. Should be https://test-example-cri.gov.uk/credential but found https://something-else.com/something',
-    );
-  });
-
-  it("should throw 'INVALID_METADATA' error when 'notification_endpoint' does not match CRI's notification endpoint", async () => {
-    const metadata = metadataBuilder().withOverrides({
-      notification_endpoint: "https://something-else.com/something",
-    });
-    await expect(
-      isValidMetadata(metadata, criUrl, authServerUrl, "jwt"),
-    ).rejects.toThrow(
-      'INVALID_METADATA: Invalid "notification_endpoint" value. Should be https://test-example-cri.gov.uk/notification but found https://something-else.com/something',
-    );
-  });
-
-  it("should throw 'INVALID_METADATA' error when 'authorization_servers' does not match the test harness URL", async () => {
+  it("should throw 'INVALID_METADATA' error when 'authorization_servers' is invalid", async () => {
     const metadata = metadataBuilder().withOverrides({
       authorization_servers: ["https://something-else.com/"],
     });
+
     await expect(
-      isValidMetadata(metadata, criUrl, authServerUrl, "jwt"),
+      isValidMetadata({
+        metadata,
+        criUrl,
+        authServerUrl,
+        credentialFormat,
+        credentialConfigurationId,
+        hasNotificationEndpoint,
+      }),
     ).rejects.toThrow(
-      'INVALID_METADATA: Invalid "authorization_servers" value. Should contain https://test-auth-server.gov.uk but only contains https://something-else.com/',
+      'INVALID_METADATA: Invalid "authorization_servers" value. Should contain https://auth.example.com but only contains https://something-else.com/',
     );
   });
 
-  it("should throw 'INVALID_METADATA' error when 'mdoc_iacas_uri' is missing - mDOC credential", async () => {
-    const metadata = metadataBuilder().withDefaults();
-    await expect(
-      isValidMetadata(metadata, criUrl, authServerUrl, "mdoc"),
-    ).rejects.toThrow(
-      'INVALID_METADATA: Invalid "mdoc_iacas_uri" value. Should be https://test-example-cri.gov.uk/iacas but found undefined',
-    );
-  });
-
-  it("should throw 'INVALID_METADATA' error when 'mdoc_iacas_uri' does not match CRI's IACAs endpoint - mDOC credential", async () => {
+  it("should throw 'INVALID_METADATA' error when 'credential_endpoint' is invalid", async () => {
     const metadata = metadataBuilder().withOverrides({
-      mdoc_iacas_uri: "https://something-else.com/something",
+      credential_endpoint: "https://something-else.com/something",
     });
+
     await expect(
-      isValidMetadata(metadata, criUrl, authServerUrl, "mdoc"),
+      isValidMetadata({
+        metadata,
+        criUrl,
+        authServerUrl,
+        credentialFormat,
+        credentialConfigurationId,
+        hasNotificationEndpoint,
+      }),
     ).rejects.toThrow(
-      'INVALID_METADATA: Invalid "mdoc_iacas_uri" value. Should be https://test-example-cri.gov.uk/iacas but found https://something-else.com/something',
+      'INVALID_METADATA: Invalid "credential_endpoint" value. Should be https://cri.example.com/credential but found https://something-else.com/something',
     );
+  });
+
+  it("should throw 'INVALID_METADATA' error when credential is not in 'credential_configurations_supported'", async () => {
+    const metadata = metadataBuilder().withOverrides({
+      credential_configurations_supported: {
+        UnknownCredential: {},
+      },
+    });
+
+    await expect(
+      isValidMetadata({
+        metadata,
+        criUrl,
+        authServerUrl,
+        credentialFormat,
+        credentialConfigurationId,
+        hasNotificationEndpoint,
+      }),
+    ).rejects.toThrow(
+      'INVALID_METADATA: Invalid "credential_configurations_supported" value. Missing credential TestCredential',
+    );
+  });
+
+  describe("given the credential issuer implements the notification endpoint", () => {
+    it("should throw 'INVALID_METADATA' error when 'notification_endpoint' is mising", async () => {
+      const metadata = metadataBuilder().withOverrides({
+        notification_endpoint: undefined,
+      });
+
+      await expect(
+        isValidMetadata({
+          metadata,
+          criUrl,
+          authServerUrl,
+          credentialFormat,
+          credentialConfigurationId,
+          hasNotificationEndpoint: true,
+        }),
+      ).rejects.toThrow(
+        "INVALID_METADATA: Invalid metadata. Missing notification_endpoint",
+      );
+    });
+
+    it("should throw 'INVALID_METADATA' error when 'notification_endpoint' is invalid", async () => {
+      const metadata = metadataBuilder().withOverrides({
+        notification_endpoint: "https://something-else.com/something",
+      });
+
+      await expect(
+        isValidMetadata({
+          metadata,
+          criUrl,
+          authServerUrl,
+          credentialFormat,
+          credentialConfigurationId,
+          hasNotificationEndpoint: true,
+        }),
+      ).rejects.toThrow(
+        'INVALID_METADATA: Invalid "notification_endpoint" value. Should be https://cri.example.com/notification but found https://something-else.com/something',
+      );
+    });
+
+    it("should return true when 'notification_endpoint' is valid", async () => {
+      const metadata = metadataBuilder().withOverrides({
+        notification_endpoint: criUrl + "/notification",
+      });
+
+      expect(
+        await isValidMetadata({
+          metadata,
+          criUrl,
+          authServerUrl,
+          credentialFormat,
+          credentialConfigurationId,
+          hasNotificationEndpoint: true,
+        }),
+      ).toEqual(true);
+    });
+  });
+
+  describe("given the credential format is mDoc ('mdoc')", () => {
+    it("should throw 'INVALID_METADATA' error when 'mdoc_iacas_uri' is missing", async () => {
+      const metadata = metadataBuilder().withDefaults();
+
+      await expect(
+        isValidMetadata({
+          metadata,
+          criUrl,
+          authServerUrl,
+          credentialFormat: "mdoc",
+          credentialConfigurationId,
+          hasNotificationEndpoint,
+        }),
+      ).rejects.toThrow(
+        "INVALID_METADATA: Invalid metadata. Missing mdoc_iacas_uri",
+      );
+    });
+
+    it("should throw 'INVALID_METADATA' error when 'mdoc_iacas_uri' is invalid", async () => {
+      const metadata = metadataBuilder().withOverrides({
+        mdoc_iacas_uri: "https://something-else.com/something",
+      });
+
+      await expect(
+        isValidMetadata({
+          metadata,
+          criUrl,
+          authServerUrl,
+          credentialFormat: "mdoc",
+          credentialConfigurationId,
+          hasNotificationEndpoint,
+        }),
+      ).rejects.toThrow(
+        'INVALID_METADATA: Invalid "mdoc_iacas_uri" value. Should be https://cri.example.com/iacas but found https://something-else.com/something',
+      );
+    });
+
+    it("should return true when metadata is valid", async () => {
+      const metadata = metadataBuilder().withOverrides({
+        mdoc_iacas_uri: criUrl + "/iacas",
+      });
+
+      expect(
+        await isValidMetadata({
+          metadata,
+          criUrl,
+          authServerUrl,
+          credentialFormat: "mdoc",
+          credentialConfigurationId,
+          hasNotificationEndpoint,
+        }),
+      ).toEqual(true);
+    });
+  });
+
+  describe("given the credential format is JWT ('jwt')", () => {
+    it("should return true when metadata is valid", async () => {
+      const metadata = metadataBuilder().withDefaults();
+
+      expect(
+        await isValidMetadata({
+          metadata,
+          criUrl,
+          authServerUrl,
+          credentialFormat: "jwt",
+          credentialConfigurationId,
+          hasNotificationEndpoint,
+        }),
+      ).toEqual(true);
+    });
   });
 });
 
@@ -110,15 +265,10 @@ function metadataBuilder<T>(): {
   withOverrides(overrides: T);
 } {
   const defaults = {
-    credential_endpoint: criUrl + "/credential",
-    authorization_servers: [authServerUrl],
     credential_issuer: criUrl,
-    credential_configurations_supported: {
-      DummyCredential: {
-        format: "jwt_vc_json",
-        types: ["VerifiableCredential", "DummyCredential"],
-      },
-    },
+    authorization_servers: [authServerUrl],
+    credential_endpoint: criUrl + "/credential",
+    credential_configurations_supported: { TestCredential: {} },
   };
   return {
     withDefaults() {
--- test/helpers/metadata/isValidMetadata.ts
+++ test/helpers/metadata/isValidMetadata.ts
@@ -1,33 +1,67 @@
 import { metadataSchema } from "./metadataSchema";
 import { getAjvInstance } from "../ajv/ajvInstance";
+import { credentialConfigurationsSupportedSchema } from "./credentialConfigurationsSupportedSchema";
 
-export interface Metadata {
+interface Metadata {
+  credential_issuer: string;
+  authorization_servers: string[];
   credential_endpoint: string;
+  credential_configurations_supported: CredentialConfigurationsSupported;
   notification_endpoint?: string;
   mdoc_iacas_uri?: string;
-  authorization_servers: string[];
-  credential_issuer: string;
-  credential_configurations_supported: object;
+}
+type CredentialConfigurationsSupported = Record<
+  string,
+  CredentialConfiguration
+>;
+
+export interface CredentialConfiguration {
+  format: "jwt_vc_json" | "mso_mdoc";
+  cryptographic_binding_methods_supported: string[];
+  credential_signing_alg_values_supported: string[];
+  credential_validity_period_max_days: number;
+  credential_refresh_web_journey_url?: string;
+  doctype?: string;
+  credential_definition?: {
+    type: string[];
+  };
+  proof_types_supported?: {
+    jwt: {
+      proof_signing_alg_values_supported: string[];
+    };
+  };
 }
 
-export async function isValidMetadata(
-  metadata: Metadata,
-  criUrl: string,
-  authServerUrl: string,
-  credentialFormat: string,
-): Promise<true> {
+export interface IsValidMetadata {
+  metadata: unknown;
+  criUrl: string;
+  authServerUrl: string;
+  credentialFormat: string;
+  credentialConfigurationId: string;
+  hasNotificationEndpoint: boolean;
+}
+
+export async function isValidMetadata({
+  metadata,
+  criUrl,
+  authServerUrl,
+  credentialFormat,
+  credentialConfigurationId,
+  hasNotificationEndpoint,
+}: IsValidMetadata): Promise<true> {
   const ajv = getAjvInstance();
-  const rulesValidator = ajv.compile(metadataSchema);
-  if (!rulesValidator(metadata)) {
-    const validationErrors = rulesValidator.errors;
+
+  const validator = ajv
+    .addSchema(
+      credentialConfigurationsSupportedSchema,
+      "credentialConfigurationsSupported",
+    )
+    .compile<Metadata>(metadataSchema);
+
+  if (!validator(metadata)) {
+    const validationErrors = validator.errors;
     throw new Error(
       `INVALID_METADATA: Metadata does not comply with the schema. ${JSON.stringify(validationErrors)}`,
-    );
-  }
-
-  if (!metadata.authorization_servers.includes(authServerUrl)) {
-    throw new Error(
-      `INVALID_METADATA: Invalid "authorization_servers" value. Should contain ${authServerUrl} but only contains ${metadata.authorization_servers}`,
     );
   }
 
@@ -37,32 +71,54 @@ export async function isValidMetadata(
     );
   }
 
-  const validCredentialEndpoint = criUrl + "/credential";
-  if (metadata.credential_endpoint !== validCredentialEndpoint) {
+  if (!metadata.authorization_servers.includes(authServerUrl)) {
     throw new Error(
-      `INVALID_METADATA: Invalid "credential_endpoint" value. Should be ${validCredentialEndpoint} but found ${metadata.credential_endpoint}`,
+      `INVALID_METADATA: Invalid "authorization_servers" value. Should contain ${authServerUrl} but only contains ${metadata.authorization_servers}`,
     );
   }
 
-  const validNotificationEndpoint = criUrl + "/notification";
+  const credentialEndpoint = criUrl + "/credential";
+  if (metadata.credential_endpoint !== credentialEndpoint) {
+    throw new Error(
+      `INVALID_METADATA: Invalid "credential_endpoint" value. Should be ${credentialEndpoint} but found ${metadata.credential_endpoint}`,
+    );
+  }
+
   if (
-    metadata.notification_endpoint &&
-    metadata.notification_endpoint !== validNotificationEndpoint
+    metadata.credential_configurations_supported[credentialConfigurationId] ===
+    undefined
   ) {
     throw new Error(
-      `INVALID_METADATA: Invalid "notification_endpoint" value. Should be ${validNotificationEndpoint} but found ${metadata.notification_endpoint}`,
+      `INVALID_METADATA: Invalid "credential_configurations_supported" value. Missing credential ${credentialConfigurationId}`,
     );
+  }
+
+  if (hasNotificationEndpoint) {
+    const notificationEndpoint = criUrl + "/notification";
+    if (!metadata.notification_endpoint) {
+      throw new Error(
+        "INVALID_METADATA: Invalid metadata. Missing notification_endpoint",
+      );
+    }
+
+    if (metadata.notification_endpoint !== notificationEndpoint) {
+      throw new Error(
+        `INVALID_METADATA: Invalid "notification_endpoint" value. Should be ${notificationEndpoint} but found ${metadata.notification_endpoint}`,
+      );
+    }
   }
 
   if (credentialFormat === "mdoc") {
-    // mdoc_iacas_uri is required in mDoc credentials only
-    const validIacasEndpoint = criUrl + "/iacas";
-    if (
-      !metadata.mdoc_iacas_uri ||
-      metadata.mdoc_iacas_uri !== validIacasEndpoint
-    ) {
+    const iacasEndpoint = criUrl + "/iacas";
+    if (!metadata.mdoc_iacas_uri) {
       throw new Error(
-        `INVALID_METADATA: Invalid "mdoc_iacas_uri" value. Should be ${validIacasEndpoint} but found ${metadata.mdoc_iacas_uri}`,
+        "INVALID_METADATA: Invalid metadata. Missing mdoc_iacas_uri",
+      );
+    }
+
+    if (metadata.mdoc_iacas_uri !== iacasEndpoint) {
+      throw new Error(
+        `INVALID_METADATA: Invalid "mdoc_iacas_uri" value. Should be ${iacasEndpoint} but found ${metadata.mdoc_iacas_uri}`,
       );
     }
   }
