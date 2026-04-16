c7740fd5a711ad81b63773d2ef90693fd9a3f6f5
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-04-16 13:53:02 +0100

    chore: remove comments for testing

--- src/server.ts
+++ src/server.ts
@@ -41,5 +41,3 @@ export const server = app
   .on("error", (error: Error) => {
     console.log(error, "Unable to start server");
   });
-// comment to test pre-commit-hooks
-// add another comment for testing
